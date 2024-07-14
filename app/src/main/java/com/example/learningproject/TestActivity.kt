25c5fbcc28316de5db51c23c6216273169b33331
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-14 10:32:03 +0330

    first commit

---
+++ app/src/main/java/com/example/learningproject/TestActivity.kt
@@ -0,0 +1,15 @@
+package com.example.learningproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.learningproject.databinding.ActivityTestBinding
+
+class TestActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTestBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnPrevious.setOnClickListener { finishAffinity() }
+    }
+}
