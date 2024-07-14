7aa310c67fed3b07676e82aafe7eef154d6f5fc7
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-14 10:38:37 +0330

    enter on main activity

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.learningproject.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
