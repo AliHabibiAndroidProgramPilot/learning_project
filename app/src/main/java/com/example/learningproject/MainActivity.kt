c06c9dbf7e7fe3497b291b5fa6dcab54f26b74db
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-17 13:43:06 +0330

    change device media sound using seek bar

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.learningproject
 
 import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.example.learningproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +17,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnClike.setOnClickListener { binding.progressBar.progress = binding.seekBar.progress }
+
+        val audioManager: AudioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        binding.seekBar.max = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        binding.seekBar.progress = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, p1, 0)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+        })
     }
 }
