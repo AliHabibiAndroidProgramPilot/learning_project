b9ee9fbd0ed3c3390f162bd1a4f41e7e8a3719b3
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-18 12:44:13 +0330

    using Rating bar and listener related to it

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.learningproject
 
-import android.content.Context
-import android.content.Intent
-import android.media.AudioManager
+import android.content.SharedPreferences
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import com.example.learningproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,20 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val audioManager: AudioManager = getSystemService(AUDIO_SERVICE) as AudioManager
-        binding.seekBar.max = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
-        binding.seekBar.progress = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, p1, 0)
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-            }
-        })
+        val sharedPreferences: SharedPreferences = getSharedPreferences("userRating", MODE_PRIVATE)
+        binding.rbStarts.rating = sharedPreferences.getFloat("rate", 0.0f)
+        binding.rbStarts.setOnRatingBarChangeListener { _, rating, _ ->
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putFloat("rate", rating)
+            editor.apply()
+            Toast.makeText(this, rating.toString(), Toast.LENGTH_LONG).show()
+        }
     }
 }
