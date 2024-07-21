69439856c4e01f6631fbb3d74ebc9184c2422972
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-21 12:33:44 +0330

    Floating Action Buttons Project

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.learningproject
 
-import android.content.SharedPreferences
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.SeekBar
-import android.widget.Toast
 import com.example.learningproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,15 +10,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var isChecked = false
-        binding.extendFloatingButton.setOnClickListener {
-            isChecked = if (isChecked) {
-                binding.extendFloatingButton.extend()
+        var isFloatingClicked = false
+        // Put Extended Floating Action Button in Shrink mode
+        binding.floatingActionButton.shrink()
+        binding.floatingActionButton.setOnClickListener {
+            // Play with "isFloatingClicked" Variable to Hide And Show Buttons
+            isFloatingClicked = if (isFloatingClicked) {
+                binding.floatingActionButton.shrink()
+                hideButtons()
                 false
             } else {
-                binding.extendFloatingButton.shrink()
+                binding.floatingActionButton.extend()
+                showButtons()
                 true
             }
         }
+        // Manage Buttons to Hide When Clicked
+        // By Using hideAfterClicked() Function And Put It On "isFloatingClicked" Variable
+        binding.shareButton.setOnClickListener {
+            isFloatingClicked = hideAfterClicked()
+        }
+        binding.deleteButton.setOnClickListener {
+            isFloatingClicked = hideAfterClicked()
+        }
+        binding.addButton.setOnClickListener {
+            isFloatingClicked = hideAfterClicked()
+        }
+    }
+
+    private fun hideAfterClicked(): Boolean {
+        // Shrink, Hide Buttons and Send False State to "isFloatingClicked" Variable
+        binding.floatingActionButton.shrink()
+        hideButtons()
+        return false
+    }
+
+    private fun showButtons() {
+        // Show Buttons Using show()
+        binding.addButton.show()
+        binding.deleteButton.show()
+        binding.shareButton.show()
+    }
+
+    private fun hideButtons() {
+        // Hide Buttons Using hide()
+        binding.addButton.hide()
+        binding.deleteButton.hide()
+        binding.shareButton.hide()
     }
 }
