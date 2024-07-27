7854e609844a01551b1a6b740ad5869c083955bb
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-27 16:00:12 +0330

    Land Scape Layout

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.learningproject
 
 import android.os.Bundle
-import android.os.Message
 import androidx.appcompat.app.AppCompatActivity
 import com.example.learningproject.databinding.ActivityMainBinding
-import com.example.learningproject.databinding.BottomSheetDialogBinding
-import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,16 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btn.setOnClickListener {
-            val sheetDialog = BottomSheetDialog(this)
-            val view = BottomSheetDialogBinding.inflate(layoutInflater)
-            sheetDialog.setContentView(view.root)
-            sheetDialog.setCancelable(false)
-            sheetDialog.setTitle("test")
-            view.btnDismiss.setOnClickListener {
-                sheetDialog.dismiss()
-            }
-            sheetDialog.show()
-        }
     }
 }
