0c161f00046cbb9370022173c5d08f5cc903090c
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-28 16:15:15 +0330

    Media Player part 3 (syncing seekBar)

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.learningproject
 import android.graphics.drawable.Icon
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.learningproject.databinding.ActivityMainBinding
 
@@ -14,6 +16,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser) mediaPlayer?.seekTo(progress)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
         binding.fabPlay.setOnClickListener {
            setFabImage()
             playPauseMusic()
@@ -29,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         if(mediaPlayer == null)
             mediaPlayer = MediaPlayer.create(this, R.raw.test_sound)
         mediaPlayer?.start()
+        initSeekBar()
         if (fabIconState)
             mediaPlayer?.pause()
         mediaPlayer?.setOnCompletionListener {
@@ -40,6 +53,21 @@ class MainActivity : AppCompatActivity() {
         mediaPlayer?.stop()
         mediaPlayer?.release()
         mediaPlayer = null
+    }
+    private fun initSeekBar() {
+        binding.seekBar.max = mediaPlayer?.duration ?: 100
+        val handler = Handler(mainLooper)
+        handler.postDelayed(object: Runnable{
+            override fun run() {
+                try {
+                    binding.seekBar.progress = mediaPlayer!!.currentPosition
+                    handler.postDelayed(this, 20)
+                } catch (e: Exception) {
+                    binding.seekBar.progress = binding.seekBar.max
+                }
+            }
+
+        }, 0)
     }
     private fun setFabImage() {
         fabIconState = if (!fabIconState) {
