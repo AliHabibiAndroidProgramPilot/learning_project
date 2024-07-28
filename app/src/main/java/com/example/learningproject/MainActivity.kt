2a92143f9e20fd6c3946c528bfcd900dfdb56893
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-28 14:01:41 +0330

    Media Player part 2

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.learningproject
 
+import android.graphics.drawable.Icon
 import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -8,27 +9,45 @@ import com.example.learningproject.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var mediaPlayer: MediaPlayer? = null
+    private var fabIconState = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.fabPlay.setOnClickListener { playMusic() }
-        binding.fabPause.setOnClickListener { pauseMusic() }
-        binding.fabStop.setOnClickListener { stopMusic() }
+        binding.fabPlay.setOnClickListener {
+           setFabImage()
+            playPauseMusic()
+        }
+        binding.fabStop.setOnClickListener {
+            stopMusic()
+            fabIconState = false
+            setFabImage()
+        }
     }
 
-    private fun playMusic() {
+    private fun playPauseMusic() {
         if(mediaPlayer == null)
             mediaPlayer = MediaPlayer.create(this, R.raw.test_sound)
-        mediaPlayer?.setOnCompletionListener { stopMusic() }
         mediaPlayer?.start()
+        if (fabIconState)
+            mediaPlayer?.pause()
+        mediaPlayer?.setOnCompletionListener {
+            stopMusic()
+            setFabImage()
+        }
     }
-
-    private fun pauseMusic() { mediaPlayer?.pause() }
-
     private fun stopMusic() {
         mediaPlayer?.stop()
         mediaPlayer?.release()
         mediaPlayer = null
+    }
+    private fun setFabImage() {
+        fabIconState = if (!fabIconState) {
+            binding.fabPlay.setImageIcon(Icon.createWithResource(this, R.drawable.ic_play))
+            true
+        } else {
+            binding.fabPlay.setImageIcon(Icon.createWithResource(this, R.drawable.ic_pause))
+            false
+        }
     }
 }
