bb9a642e690438bf1fd02460dcd8b04f23ab69a0
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-31 12:31:17 +0330

    Usage of Save Instance State

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -5,87 +5,29 @@ import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
+import android.text.Editable
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.learningproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var mediaPlayer: MediaPlayer? = null
-    private var fabIconState = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(p0: SeekBar?, progress: Int, fromUser: Boolean) {
-                if(fromUser) mediaPlayer?.seekTo(progress)
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {}
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {}
-
-        })
-        binding.fabPlay.setOnClickListener {
-           setFabImage()
-            playPauseMusic()
-        }
-        binding.fabStop.setOnClickListener {
-            stopMusic()
-            fabIconState = false
-            setFabImage()
+        binding.button.setOnClickListener {
+            binding.textView.text = binding.editText.text.toString()
+            binding.editText.text = Editable.Factory().newEditable("")
         }
     }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("12", binding.textView.text.toString())
+    }
 
-    private fun playPauseMusic() {
-        if(mediaPlayer == null) {
-            //mediaPlayer = MediaPlayer.create(this, R.raw.test_sound)
-            mediaPlayer = MediaPlayer()
-            mediaPlayer?.setAudioAttributes( AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                    .setUsage(AudioAttributes.USAGE_MEDIA)
-                    .build()
-            )
-            mediaPlayer?.setDataSource("https://static1.squarespace.com/static/5abcedbb5417fcb65ba53771/t/600219d91222a2448d95f58f/1611007518392/Soul+Food+MP3.mp3/original/Soul+Food+MP3.mp3?download=true")
-            mediaPlayer?.prepare()
-        }
-        mediaPlayer?.start()
-        initSeekBar()
-        if (fabIconState)
-            mediaPlayer?.pause()
-        mediaPlayer?.setOnCompletionListener {
-            stopMusic()
-            setFabImage()
-        }
-    }
-    private fun stopMusic() {
-        mediaPlayer?.stop()
-        mediaPlayer?.release()
-        mediaPlayer = null
-    }
-    private fun initSeekBar() {
-        binding.seekBar.max = mediaPlayer?.duration ?: 100
-        val handler = Handler(mainLooper)
-        handler.postDelayed(object: Runnable{
-            override fun run() {
-                try {
-                    binding.seekBar.progress = mediaPlayer!!.currentPosition
-                    handler.postDelayed(this, 20)
-                } catch (e: Exception) {
-                    binding.seekBar.progress = binding.seekBar.max
-                }
-            }
-
-        }, 0)
-    }
-    private fun setFabImage() {
-        fabIconState = if (!fabIconState) {
-            binding.fabPlay.setImageIcon(Icon.createWithResource(this, R.drawable.ic_play))
-            true
-        } else {
-            binding.fabPlay.setImageIcon(Icon.createWithResource(this, R.drawable.ic_pause))
-            false
-        }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        binding.textView.text = savedInstanceState.getString("12")
     }
 }
