6ddcccab1590bd643cdb535716f81d03e9df975f
Ali Habibi <biz.alihabibi@gmail.com>
2024-07-31 14:33:08 +0330

    Video View and set up Controller for it

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.learningproject
 import android.graphics.drawable.Icon
 import android.media.AudioAttributes
 import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.text.Editable
+import android.widget.MediaController
 import android.widget.SeekBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.learningproject.databinding.ActivityMainBinding
 
@@ -16,18 +19,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener {
-            binding.textView.text = binding.editText.text.toString()
-            binding.editText.text = Editable.Factory().newEditable("")
-        }
-    }
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString("12", binding.textView.text.toString())
+        initVideoView()
     }
 
+    private fun initVideoView() {
+        val uri = Uri.parse("android.resource://${packageName}/${R.raw.testvideo}")
+        val controller = MediaController(this)
+        try {
+            binding.videoView.setVideoURI(uri)
+            binding.videoView.setMediaController(MediaController(this))
+            binding.videoView.start()
+        } catch (e: Exception) {
+            Toast.makeText(this, "${e.cause}", Toast.LENGTH_LONG).show()
+        }
+        binding.videoView.setOnPreparedListener{
+            it.setOnVideoSizeChangedListener { _, _, _ ->
+                controller.setAnchorView(binding.videoView)
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        binding.textView.text = savedInstanceState.getString("12")
     }
 }
