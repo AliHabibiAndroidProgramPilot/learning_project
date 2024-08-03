b60b5f69f1279ac6ea6a7b6adac5ecc7ace24c6c
Ali Habibi <biz.alihabibi@gmail.com>
2024-08-03 14:27:17 +0330

    Usage of Calendar View

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.learningproject
 
-import android.net.Uri
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.text.Editable
-import android.widget.MediaController
-import android.widget.VideoView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.learningproject.databinding.ActivityMainBinding
+import java.lang.reflect.Array.set
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,29 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initVideoView()
-    }
 
-    private fun initVideoView() {
-        binding.videoView.apply {
-            val controller = MediaController(this@MainActivity)
-            setVideoURI(Uri.parse("android.resource://${packageName}/${R.raw.testvideo}"))
-            setMediaController(controller)
-            start()
-            setOnPreparedListener {
-                it.setOnVideoSizeChangedListener { _, _, _ ->
-                    controller.setAnchorView(binding.videoView)
-                }
-            }
+        binding.calendarView.setOnDateChangeListener { _, year, month, day ->
+            binding.textView.text = "$year/$day/${month + 1}"
         }
-    }
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putInt("videoPosition", binding.videoView.currentPosition)
-        super.onSaveInstanceState(outState)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        binding.videoView.seekTo(savedInstanceState.getInt("videoPosition"))
-        super.onRestoreInstanceState(savedInstanceState)
     }
 }
