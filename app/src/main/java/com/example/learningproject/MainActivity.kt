82d89533a6cedeac0a6d04ca00092109836bf566
Ali Habibi <biz.alihabibi@gmail.com>
2024-08-03 16:56:04 +0330

    Showing Date Picker Dialog

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.learningproject
 
-import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.learningproject.databinding.ActivityMainBinding
-import java.lang.reflect.Array.set
 import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
@@ -14,9 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.calendarView.setOnDateChangeListener { _, year, month, day ->
-            binding.textView.text = "$year/$day/${month + 1}"
+        binding.button.setOnClickListener {
+            val todayDate = Calendar.getInstance()
+            val datePickerDialog = DatePickerDialog(
+                this,
+                { _, year, month, day ->
+                    // Block codes occur when OK button pushed
+                },
+                todayDate.get(Calendar.YEAR),
+                todayDate.get(Calendar.MONTH),
+                todayDate.get(Calendar.DAY_OF_MONTH)
+            )
+            datePickerDialog.show()
         }
     }
 }
