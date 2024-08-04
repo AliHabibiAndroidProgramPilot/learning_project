1f8fd98a7d861ebabbe3b1323f21e8bdfa0c9e58
Ali Habibi <biz.alihabibi@gmail.com>
2024-08-04 12:32:34 +0330

    Usage of Time Picker Dialog

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -1,23 +1,33 @@
 package com.example.learningproject
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.learningproject.databinding.ActivityMainBinding
-import java.util.Calendar
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.timePicker.apply {
-            setIs24HourView(true)
-            setOnTimeChangedListener { _, hour, minute ->
-                binding.textView.text = "$hour : $minute"
-            }
-    }
+        binding.button.setOnClickListener {
+            val todayDate = Calendar.getInstance()
+            val timePickerDialog = TimePickerDialog(
+                this,
+                { _, hour, minute ->
+                    binding.textView.text = "$hour : $minute"
+                },
+                todayDate.get(Calendar.HOUR_OF_DAY),
+                todayDate.get(Calendar.MINUTE),
+                true
+            )
+            timePickerDialog.show()
+        }
 }
 }
