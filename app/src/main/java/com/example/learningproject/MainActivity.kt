d4322497b467794a7797ef888e7e9d407504f2f2
Ali Habibi <biz.alihabibi@gmail.com>
2024-08-04 14:44:21 +0330

    Modify Fonts

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.learningproject
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -11,23 +12,15 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.button.setOnClickListener {
-            val todayDate = Calendar.getInstance()
-            val timePickerDialog = TimePickerDialog(
-                this,
-                { _, hour, minute ->
-                    binding.textView.text = "$hour : $minute"
-                },
-                todayDate.get(Calendar.HOUR_OF_DAY),
-                todayDate.get(Calendar.MINUTE),
-                true
-            )
-            timePickerDialog.show()
+            val typeFace = Typeface.createFromAsset(assets, "iran_sans.tff")
+            binding.textView.typeface = typeFace
         }
-}
+    }
 }
