aa8b543579d6901540730083cdfcc935614fb0e5
Ali <biz.alihabibi@gmail.com>
2024-08-11 12:13:40 +0330

    Implementation of custom tool bar

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.learningproject
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -14,36 +15,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnTest.setOnClickListener { startActivity(Intent(this, TestActivity::class.java)) }
+        // Define our toolbar as action bar to Android
+        setSupportActionBar(binding.mainToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // Hides default title of action bar
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.title = "test title"
+        supportActionBar?.subtitle = "test subtitle"
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
-            R.id.delete_item -> {
-                Toast.makeText(this, "delete", Toast.LENGTH_LONG).show()
-                true
-            }
-            R.id.Exit_item -> {
-                Toast.makeText(this, "Exit", Toast.LENGTH_LONG).show()
-                true
-            }
-            R.id.share_item -> {
-                Toast.makeText(this, "share", Toast.LENGTH_LONG).show()
-                true
-            }
-            R.id.rename_item -> {
-                Toast.makeText(this, "rename", Toast.LENGTH_LONG).show()
-                true
-            }
-            R.id.report_item -> {
-                Toast.makeText(this, "report", Toast.LENGTH_LONG).show()
-                true
-            }
-            else -> false
-        }
     }
 }
