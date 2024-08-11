6b9f9cbdbe4563bef935cddd435301d4a131492e
Ali <biz.alihabibi@gmail.com>
2024-08-11 16:37:51 +0330

    Animations with anim Directory

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.learningproject
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.learningproject.databinding.ActivityMainBinding
@@ -15,13 +19,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btn.setOnClickListener {
-            /*
-            binding.img.animate().alpha(1f).duration = 35000
-            binding.img.animate().translationX(120f).duration = 15000
-            binding.img.animate().translationY(120f).duration = 15000
-            binding.img.animate().scaleY(2f).duration = 15000
-            */
+
+    }
+    fun onClick(view: View) {
+        when(view.id) {
+            R.id.btn_scale -> {
+                val anim = AnimationUtils.loadAnimation(this, R.anim.scale)
+                anim.fillAfter = true
+                anim.duration = 1500
+                binding.img.startAnimation(anim)
+
+            }
+            R.id.btn_rotate -> {
+                val anim = AnimationUtils.loadAnimation(this, R.anim.rotate)
+                anim.fillAfter = true
+                anim.duration = 1500
+                binding.img.startAnimation(anim)
+            }
+            R.id.btn_scaleRotate -> {
+                val anim = AnimationUtils.loadAnimation(this, R.anim.scale_rotate)
+                anim.fillAfter = true
+                anim.duration = 2000
+                binding.img.startAnimation(anim)
+            }
+            R.id.btn_Fadein -> {
+                val anim = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+                anim.fillAfter = true
+                anim.duration = 1500
+                binding.img.startAnimation(anim)
+            }
+            R.id.btn_fadeout -> {
+                val anim = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+                anim.fillAfter = true
+                anim.duration = 1500
+                binding.img.startAnimation(anim)
+            }
+            R.id.btn_translate -> {
+                val anim = AnimationUtils.loadAnimation(this, R.anim.translate)
+                anim.fillAfter = true
+                anim.duration = 1500
+                binding.img.startAnimation(anim)
+            }
         }
     }
 }
