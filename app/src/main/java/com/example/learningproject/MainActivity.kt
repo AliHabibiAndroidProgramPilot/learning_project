ff32c06e2a5b9edbbc424f6ec0ab5dccb3ad62d5
Ali <biz.alihabibi@gmail.com>
2024-08-24 12:51:34 +0330

    Implement Navigation Drawer part1

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -1,18 +1,35 @@
 package com.example.learningproject
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.learningproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    /*private val drawerLayout: DrawerLayout
+        get() = binding.drawableLayoutParent
+    private val actionBarDrawerToggle: ActionBarDrawerToggle
+        get() = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //Change theme to Dark mode
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        setSupportActionBar(binding.toolBar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.drawableLayoutParent,
+            binding.toolBar,
+            R.string.nav_open,
+            R.string.nav_close
+        )
+        actionBarDrawerToggle.isDrawerIndicatorEnabled = true
+        binding.drawableLayoutParent.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
     }
 }
