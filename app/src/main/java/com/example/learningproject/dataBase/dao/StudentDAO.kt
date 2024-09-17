fb1776b6e19d82dc89193d93e6f2ffa117065d36
Ali <biz.alihabibi@gmail.com>
2024-09-17 10:42:50 +0330

    Get State of Saving(Success or Failure In Save Data)

--- app/src/main/java/com/example/learningproject/dataBase/dao/StudentDAO.kt
+++ app/src/main/java/com/example/learningproject/dataBase/dao/StudentDAO.kt
@@ -7,14 +7,16 @@ import com.example.learningproject.dataBase.dataModel.StudentDataModel
 class StudentDAO(
     private val dataBaseWritable: DataBaseHelper
 ) {
-    fun insertStudent(student: StudentDataModel) {
+    fun insertStudent(student: StudentDataModel): Boolean {
         val database = dataBaseWritable.writableDatabase
         val contentValues = ContentValues()
         contentValues.put(DataBaseHelper.STUDENT_NAME, student.studentName)
         contentValues.put(DataBaseHelper.STUDENT_FAMILY, student.studentFamily)
         contentValues.put(DataBaseHelper.STUDENT_TEACHER_ID, student.studentTeacherId)
         contentValues.put(DataBaseHelper.STUDENT_AGE, student.studentAge)
-        database.insert(DataBaseHelper.STUDENT_TABLE, null, contentValues)
+        val insertDataResult =
+            database.insert(DataBaseHelper.STUDENT_TABLE, null, contentValues)
         database.close()
+        return insertDataResult > 0
     }
 }
--- app/src/main/java/com/example/learningproject/dataBase/dao/TeacherDAO.kt
+++ app/src/main/java/com/example/learningproject/dataBase/dao/TeacherDAO.kt
@@ -7,13 +7,15 @@ import com.example.learningproject.dataBase.dataModel.TeacherDataModel
 class TeacherDAO(
     private val dbWritable: DataBaseHelper
 ) {
-    fun insertTeacher(teacher: TeacherDataModel) {
+    fun insertTeacher(teacher: TeacherDataModel): Boolean{
         val dataBase = dbWritable.writableDatabase
         val contentValues = ContentValues()
         contentValues.put(DataBaseHelper.TEACHER_NAME, teacher.teacherName)
         contentValues.put(DataBaseHelper.TEACHER_FAMILY, teacher.teacherFamily)
         contentValues.put(DataBaseHelper.TEACHER_NATIONAL_CODE, teacher.teacherNationalCode)
-        dataBase.insert(DataBaseHelper.TEACHER_TABLE, null, contentValues)
+        val insertDataResult =
+            dataBase.insert(DataBaseHelper.TEACHER_TABLE, null, contentValues)
         dataBase.close()
+        return insertDataResult > 0
     }
 }
