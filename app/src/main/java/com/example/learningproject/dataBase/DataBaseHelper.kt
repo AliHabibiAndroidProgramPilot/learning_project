33fa1041593ff932eede839017f77a99dcc7f871
Ali <biz.alihabibi@gmail.com>
2024-10-03 16:23:31 +0330

    Upgrade Data Base part 1

--- app/src/main/java/com/example/learningproject/dataBase/DataBaseHelper.kt
+++ app/src/main/java/com/example/learningproject/dataBase/DataBaseHelper.kt
@@ -8,7 +8,7 @@ class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DB_NAME, null
     companion object {
         //Data Base Info
         private const val DB_NAME = "Test_school.dataBase"
-        private const val DB_VERSION = 1
+        private const val DB_VERSION = 2
         //Student Table
         const val STUDENT_TABLE = "Student"
         const val STUDENT_ID = "Id"
@@ -22,6 +22,7 @@ class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DB_NAME, null
         const val TEACHER_NAME = "Name"
         const val TEACHER_FAMILY = "Family"
         const val TEACHER_NATIONAL_CODE = "National_Code"
+        const val TEACHER_AGE = "Age"
     }
 
     override fun onCreate(dataBase: SQLiteDatabase?) {
@@ -41,9 +42,7 @@ class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DB_NAME, null
     }
 
     override fun onUpgrade(dataBase: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        //Matters which Table Must Be Delete First
-        dataBase?.execSQL("DROP TABLE IF EXISTS $STUDENT_TABLE")
-        dataBase?.execSQL("DROP TABLE IF EXISTS $TEACHER_TABLE")
-        onCreate(dataBase)
+        dataBase?.execSQL("ALTER TABLE $TEACHER_TABLE " +
+                "ADD COLUMN $TEACHER_AGE INTEGER DEFAULT 0")
     }
 }
