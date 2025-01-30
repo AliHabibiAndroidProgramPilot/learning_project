67604f660a010ddb7c904b9315a506b13e21fbf0
Ali <biz.alihabibi@gmail.com>
2025-01-30 20:16:30 +0330

    Add Picasso Library

--- app/src/main/java/com/example/learningproject/MainActivity.kt
+++ app/src/main/java/com/example/learningproject/MainActivity.kt
@@ -7,38 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.learningproject.databinding.ActivityMainBinding
 import com.example.learningproject.model.MainModel
 
-class MainActivity : AppCompatActivity(), ApiRespond {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btn.setOnClickListener {
-            it.isEnabled = false
-            binding.progressBar.visibility = View.VISIBLE
-            ApiRepository.instance.sendText(
-                "bYc92CJsQu6utThQ1ZBwwqhTsXh6BTkkzmMn2APv",
-                "No Limit I'm Fucking Solider",
-                this
-            )
-        }
-    }
-
-    override fun onRespond(respond: MainModel) {
-        binding.txtApiRespond.text = respond.message
-        binding.progressBar.visibility = View.INVISIBLE
-        binding.btn.isEnabled = true
-    }
-
-    override fun onNotRespond(respond: String) {
-
-    }
-
-    override fun onRespondFailure(error: String?) {
-        if (error != null) {
-            Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
-            binding.progressBar.visibility = View.INVISIBLE
-            binding.btn.isEnabled = true
-        }
     }
 }
